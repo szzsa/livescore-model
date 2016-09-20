9c6980f56f257b03477959dc98569e426a95a59e
Attila Szasz <attila.szasz@iquestgroup.com>
2016-09-20 10:17:06 +0300

    Add goal types

---
+++ src/main/java/ro/szzsa/livescore/model/GoalType.java
@@ -0,0 +1,32 @@
+package ro.szzsa.livescore.model;
+
+/**
+ *
+ */
+public enum GoalType {
+
+  /**
+   * Goal scored when both teams have the same amount of skater are on the ice.
+   */
+  EQ,
+
+  /**
+   * Goal scored by a team that has one more skater on ice than its opponent.
+   */
+  PP1,
+
+  /**
+   * Goal scored by a team that has two more skaters on ice than its opponent.
+   */
+  PP2,
+
+  /**
+   * Goal scored by a team that has one fewer skater on ice than its opponent.
+   */
+  SH1,
+
+  /**
+   * Goal scored by a team that has two fewer skaters on ice than its opponent.
+   */
+  SH2
+}
